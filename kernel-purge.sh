62bf90c7ef1f05afda0bbbde2c57b1f17b1baa47
tlafrank <43323190+tlafrank@users.noreply.github.com>
2018-09-17 06:10:53 +1000

    Allow user to select kernel
    Allows the user to select the kernel to remove, one at a time

--- kernel-purge.sh
+++ kernel-purge.sh
@@ -5,21 +5,41 @@
 current_kernel="$(uname -r | sed 's/\(.*\)-\([^0-9]\+\)/\1/')"
 current_ver=${current_kernel/%-generic}
 
+#Display the current running kernel version
 echo "Running kernel version is: ${current_kernel}"
-# uname -a
+
+#Display the currently installed kernel packages which are unused
+echo 'Installed and unusued kernel packages:'
+dpkg -l 'linux-*' | sed '/^ii/!d;/libc-dev/d;/'${current_ver}'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d'
+
+#Ask the user which one they wish to delete
+read -p 'Which version do you want to remove (I.e. 4.15.0-33)? ' -r
+todelete=$REPLY
+
+#Check that the version exists, otherwise exit
+exists="$(dpkg -l 'linux-*' | sed '/^ii/!d;/'${current_ver}'/d;/-'${todelete}'-/!d' | wc -l)"
+
+if [[ $exists > 0 ]]
+then
+  #Version exists, confirm that the version is to be deleted
+  read -p ${todelete}' exists. Are you sure [y/n]? ' -n 1 -r
+  printf '\n'
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    echo 'Deleting '${todelete}
+    #dpkg -l 'linux-*' | sed '/^ii/!d;/libc-dev/d;/'${todelete}'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
+    dpkg -l 'linux-*' | sed '/^ii/!d;/libc-dev/d;/'${todelete}'/!d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge 
+    echo ${todelete} 'deleted'
+  else
+    echo 'User cancelled. Nothing deleted.'
+  fi
+else
+  #Version does not exist. Throw error an quit
+  echo 'Version '${todelete}' does not exist. Exiting.'
+fi
+
 
 function xpkg_list() {
-    dpkg -l 'linux-*' | sed '/^ii/!d;/linux-libc-dev/d;/'${current_ver}'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d'
+    echo "now running the function"
+    dpkg -l 'linux-*' | sed '/^ii/!d;/linux-libc-dev/d;/'${todelete}'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d'
 }
-
-echo "The following (unused) KERNEL packages will be removed:"
-xpkg_list
-
-read -p 'Do you want to continue [yN]? ' -n 1 -r
-printf "\n"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    xpkg_list | xargs sudo apt-get -y purge
-else
-    echo 'Operation aborted. No changes were made.'
-fi
